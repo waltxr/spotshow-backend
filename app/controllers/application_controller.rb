90ac8d5e8432dee7b2af7da2a41a8c476ef75174
Adam Walter <Waltxr@users.noreply.github.com>
2018-05-06 11:22:01 -0400

    change my_user to current_user in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,14 +14,14 @@ class ApplicationController < ActionController::API
     JWT.decode(jwt_token, ENV["MY_SECRET"], true, my_algorithm)[0]
   end
 
-  def my_user_id
+  def current_user_id
     authenticate_or_request_with_http_token do |jwt_token, options|
       decoded_token = decode(jwt_token)
-      my_user_id = decoded_token[0]["user_id"]
+      current_user_id = decoded_token[0]["user_id"]
     end
   end
 
-  def my_user
+  def current_user
     authenticate_or_request_with_http_token do |jwt_token, options|
       begin
         decoded_token = decode(jwt_token)
@@ -29,30 +29,30 @@ class ApplicationController < ActionController::API
         return nil
       end
       if decoded_token["user_id"]
-        @my_user ||= User.find(decoded_token["user_id"])
+        @current_user ||= User.find(decoded_token["user_id"])
       end
     end
   end
 
   def logged_in?
-    !!my_user
+    !!current_user
   end
 
   def authorized
     if logged_in?
-      check_for_refresh(my_user)
+      check_for_refresh(current_user)
     else
       (render json: {message: "Not welcome" }, status: 401)
     end
   end
 
-  def check_for_refresh(my_user)
-    if my_user.access_token_expired?
-      refresh_token = decode(my_user.refresh_token)
+  def check_for_refresh(current_user)
+    if current_user.access_token_expired?
+      refresh_token = decode(current_user.refresh_token)
       token = refresh_token["token"]
       access_token = SpotifyAdapter.refresh_access_token(token)
       encodedAccess = issue_token({token: access_token})
-      my_user.update(access_token: encodedAccess)
+      current_user.update(access_token: encodedAccess)
     end
   end
 
